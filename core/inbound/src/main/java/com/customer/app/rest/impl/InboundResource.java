eeba49a9980c50d061410bad253b2f70d1e0f9df
Mary Cochran <marycochran@localhost.localdomain>
2015-09-22 16:41:18 -0400

    returning null

--- core/inbound/src/main/java/com/customer/app/rest/impl/InboundResource.java
+++ core/inbound/src/main/java/com/customer/app/rest/impl/InboundResource.java
@@ -3,6 +3,7 @@ package com.customer.app.rest.impl;
 import javax.ws.rs.Consumes;
 import javax.ws.rs.POST;
 import javax.ws.rs.Path;
+import javax.ws.rs.core.Response;
 
 import org.springframework.stereotype.Service;
 
@@ -15,6 +16,7 @@ public class InboundResource {
     @POST
     @Path("/inbound")
     @Consumes("application/xml")
-    public void postInbound(Patient person) {
+    public Response postInbound(Patient person) {
+        return null;
     }
 }
